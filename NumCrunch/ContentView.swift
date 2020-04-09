25963b2acc9d888be81e3950a871261c32326ddd
Ufuoma Okoro <ufookoro@gmail.com>
2020-04-09 16:17:26 +0100

    Initial Commit

---
+++ NumCrunch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NumCrunch
+//
+//  Created by Ufuoma Okoro on 09/04/2020.
+//  Copyright © 2020 Ufuoma Okoro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
